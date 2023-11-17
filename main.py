698d1f091505c6a6b81e4f40925cfac05761ab59
unknown <selim.bamri@ihec.u-carthage.tn>
2023-11-17 14:29:44 +0100

    Scraping countries & capitals

--- main.py
+++ main.py
@@ -1,16 +1,77 @@
-# This is a sample Python script.
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+driver = webdriver.Edge()
+driver.get("https://www.jetpunk.com/user-quizzes/29612/capitales-du-monde")
+start_button = driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[2]/div[2]/div[4]/button")
+start_button.click()
+time.sleep(2)
+stop_button = driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[4]/div/div/div[1]/button")
+stop_button.click()
+time.sleep(1)
+countries = []
+for i in range(2, 47):
+    country_name = driver.find_element(By.XPATH, f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/div/table/tbody/tr/td[1]/table/tbody/tr[{i}]/td[1]/div")
+    capital_name = driver.find_element(By.XPATH, f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/div/table/tbody/tr/td[1]/table/tbody/tr[{i}]/td[2]")
+    country = {
+        "Nom": country_name.text.replace("\n", ""),
+        "Capitale": capital_name.text.replace("\n", ""),
+        "Continent": "Europe"
+    }
+    countries.append(country)
 
+for i in range(49, 72):
+    country_name = driver.find_element(By.XPATH, f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/div/table/tbody/tr/td[1]/table/tbody/tr[{i}]/td[1]/div")
+    capital_name = driver.find_element(By.XPATH, f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/div/table/tbody/tr/td[1]/table/tbody/tr[{i}]/td[2]")
+    country = {
+        "Nom": country_name.text.replace("\n", ""),
+        "Capitale": capital_name.text.replace("\n", ""),
+        "Continent": "Amérique du nord"
+    }
+    countries.append(country)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+for i in range(2, 50):
+    country_name = driver.find_element(By.XPATH, f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/div/table/tbody/tr/td[2]/table/tbody/tr[{i}]/td[1]/div")
+    capital_name = driver.find_element(By.XPATH, f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/div/table/tbody/tr/td[2]/table/tbody/tr[{i}]/td[2]")
+    country = {
+        "Nom": country_name.text.replace("\n", ""),
+        "Capitale": capital_name.text.replace("\n", ""),
+        "Continent": "Asie"
+    }
+    countries.append(country)
 
+for i in range(52, 66):
+    country_name = driver.find_element(By.XPATH, f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/div/table/tbody/tr/td[2]/table/tbody/tr[{i}]/td[1]/div")
+    capital_name = driver.find_element(By.XPATH, f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/div/table/tbody/tr/td[2]/table/tbody/tr[{i}]/td[2]")
+    country = {
+        "Nom": country_name.text.replace("\n", ""),
+        "Capitale": capital_name.text.replace("\n", ""),
+        "Continent": "Océanie"
+    }
+    countries.append(country)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+for i in range(2, 14):
+    country_name = driver.find_element(By.XPATH, f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/div/table/tbody/tr/td[3]/table/tbody/tr[{i}]/td[1]/div")
+    capital_name = driver.find_element(By.XPATH, f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/div/table/tbody/tr/td[3]/table/tbody/tr[{i}]/td[2]")
+    country = {
+        "Nom": country_name.text.replace("\n", ""),
+        "Capitale": capital_name.text.replace("\n", ""),
+        "Continent": "Amérique du sud"
+    }
+    countries.append(country)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for i in range(16, 70):
+    country_name = driver.find_element(By.XPATH, f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/div/table/tbody/tr/td[3]/table/tbody/tr[{i}]/td[1]/div")
+    capital_name = driver.find_element(By.XPATH, f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/div/table/tbody/tr/td[3]/table/tbody/tr[{i}]/td[2]")
+    country = {
+        "Nom": country_name.text.replace("\n", ""),
+        "Capitale": capital_name.text.replace("\n", ""),
+        "Continent": "Afrique"
+    }
+    countries.append(country)
+
+print(len(countries))
+print(countries)
+time.sleep(5)
+
