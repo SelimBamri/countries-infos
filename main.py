267b31595e34765503680945073e39a42afa0cd1
unknown <selim.bamri@ihec.u-carthage.tn>
2023-12-05 00:53:07 +0100

    Creating new lists for maps and flags

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Edge()
+
+# Getting country names and their capitals
 driver.get("https://www.jetpunk.com/user-quizzes/29612/capitales-du-monde")
 start_button = driver.find_element(By.XPATH,
                                    "/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[2]/div[2]/div[4]/button")
@@ -86,6 +88,7 @@ for i in range(16, 70):
     countries.append(country)
 time.sleep(5)
 
+# Getting countries currencies
 driver.get("https://www.jetpunk.com/user-quizzes/1584524/devises-monetaires-par-pays")
 start_button = driver.find_element(By.XPATH,
                                    "/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[2]/div[2]/div[4]/button")
@@ -99,11 +102,11 @@ countries_currencies = []
 for i in range(2, 68):
     country_name = driver.find_element(By.XPATH,
                                        f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/div/table/tbody/tr/td[1]/table/tbody/tr[{i}]/td[1]/div")
-    capital_name = driver.find_element(By.XPATH,
-                                       f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/div/table/tbody/tr/td[1]/table/tbody/tr[{i}]/td[2]")
+    country_currency = driver.find_element(By.XPATH,
+                                           f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/div/table/tbody/tr/td[1]/table/tbody/tr[{i}]/td[2]")
     country = {
         "Nom": country_name.text.replace("\n", ""),
-        "Devise": capital_name.text.replace("\n", ""),
+        "Devise": country_currency.text.replace("\n", ""),
     }
     countries_currencies.append(country)
 
@@ -131,10 +134,70 @@ for i in range(2, 67):
     }
     countries_currencies.append(country)
 
+# Getting country flags
+driver.get("https://www.jetpunk.com/user-quizzes/176134/drapeaux-du-monde")
+start_button = driver.find_element(By.XPATH,
+                                   "//html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[2]/div[2]/div[4]/button")
+start_button.click()
+time.sleep(2)
+stop_button = driver.find_element(By.XPATH,
+                                  "/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[4]/div/div/div[1]/button")
+stop_button.click()
+time.sleep(5)
+countries_flags = []
+for i in range(1, 40):
+    for j in range(1, 6):
+        country_name = driver.find_element(By.XPATH,
+                                           f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/table[{i}]/tbody/tr[2]/td[{j}]/div")
+        country_flag = driver.find_element(By.XPATH,
+                                           f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/table[{i}]/tbody/tr[1]/td[{j}]/div/img")
+        country = {
+            "Nom": country_name.text.replace("\n", ""),
+            "Drapeau": country_flag.get_attribute("src")
+        }
+        countries_flags.append(country)
+countries_flags.append({
+    "Nom": "Gambie",
+    "Drapeau": "https://jetpunk.b-cdn.net/img/user-photo-library/08/08c6fc6253-450.png"
+})
+
+# Getting list of the maps
+driver.get("https://www.jetpunk.com/user-quizzes/6121/chaque-formes-de-pays")
+start_button = driver.find_element(By.XPATH,
+                                   "/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[2]/div[2]/div[4]/button")
+start_button.click()
+time.sleep(2)
+stop_button = driver.find_element(By.XPATH,
+                                  "/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[4]/div/div/div[1]/button")
+stop_button.click()
+time.sleep(5)
+countries_maps = []
+for i in range(1, 40):
+    for j in range(1, 6):
+        country_name = driver.find_element(By.XPATH,
+                                           f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/table[{i}]/tbody/tr[2]/td[{j}]/div")
+        country_map = driver.find_element(By.XPATH,
+                                          f"/html/body/div/div/div[2]/div[3]/div/div/div[1]/div/div[2]/div[11]/table[{i}]/tbody/tr[1]/td[{j}]/div/img")
+        country = {
+            "Nom": country_name.text.replace("\n", ""),
+            "Drapeau": country_map.get_attribute("src")
+        }
+        countries_maps.append(country)
+countries_maps.append({
+    "Nom": "Îles Marshall",
+    "Drapeau": "https://jetpunk.b-cdn.net/img/user-photo-library/f3/f3158861d3-450.png"
+})
+
+# Printing the lists
 print(countries)
 print('--------------------')
 print(countries_currencies)
 print('--------------------')
+print(countries_flags)
+print('--------------------')
+print(countries_maps)
+
+# Manually correcting the inconsistencies
 for i in range(196):
     if countries[i].get('Nom') == 'RépubliqueTchèque':
         countries[i]['Nom'] = 'République Tchèque'
@@ -150,25 +213,28 @@ for i in range(196):
         countries[i]['Nom'] = 'République démocratique du Congo'
     elif countries[i].get('Nom') == 'Rép du Congo':
         countries[i]['Nom'] = 'République du Congo'
-correspondences = 0
-is_okay = True
-for c1 in countries:
-    corr = False
-    for c2 in countries_currencies:
-        if c1.get('Nom').lower() == c2.get('Nom').lower():
-            correspondences += 1
-            corr = True
-    if not corr:
-        is_okay = False
-        print(c1.get("Nom"))
-if is_okay:
-    print('Parfait')
-
-for c in countries_currencies:
-    for x in countries:
-        if x["Nom"] == c["Nom"]:
-            x["Devise"] = c["Devise"]
-
-print(countries)
-
+    if countries_flags[i].get('Nom') == 'Etats-Unis':
+        countries_flags[i]['Nom'] = 'États-Unis'
+    elif countries_flags[i].get('Nom') == 'Saint-Christophe- et-Niévès':
+        countries_flags[i]['Nom'] = 'Saint-Christophe-et-Niévès'
+    elif countries_flags[i].get('Nom') == 'Saint-Vincent-et-les- Grenadines':
+        countries_flags[i]['Nom'] = 'Saint-Vincent-et-les-Grenadines'
+    elif countries_flags[i].get('Nom') == 'Salomon':
+        countries_flags[i]['Nom'] = 'Îles Salomon'
+    elif countries_flags[i].get('Nom') == 'Papouasie-Nouvelle- Guinée':
+        countries_flags[i]['Nom'] = 'Papouasie-Nouvelle-Guinée'
+    elif countries_flags[i].get('Nom') == 'Île Maurice':
+        countries_flags[i]['Nom'] = 'Maurice'
+    if countries_maps[i].get('Nom') == 'Etats-Unis':
+        countries_maps[i]['Nom'] = 'États-Unis'
+    elif countries_maps[i].get('Nom') == 'Saint-Christophe- et-Niévès':
+        countries_maps[i]['Nom'] = 'Saint-Christophe-et-Niévès'
+    elif countries_maps[i].get('Nom') == 'Saint-Vincent-et-les- Grenadines':
+        countries_maps[i]['Nom'] = 'Saint-Vincent-et-les-Grenadines'
+    elif countries_maps[i].get('Nom') == 'Salomon':
+        countries_maps[i]['Nom'] = 'Îles Salomon'
+    elif countries_maps[i].get('Nom') == 'Papouasie-Nouvelle- Guinée':
+        countries_maps[i]['Nom'] = 'Papouasie-Nouvelle-Guinée'
+    elif countries_maps[i].get('Nom') == 'Île Maurice':
+        countries_maps[i]['Nom'] = 'Maurice'
 
